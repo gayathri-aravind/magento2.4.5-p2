dbdbf7ecf4dd2793f1b3d3958800394a1c644bc5
Gayathri Aravind <gayathri.aravind@ziffity.com>
2023-06-26 15:34:36 +0530

    Frontpage - custom module and layout

--- app/etc/config.php
+++ app/etc/config.php
@@ -331,6 +331,7 @@ return [
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1,
-        'Training_Example' => 1
+        'Training_Example' => 1,
+        'Training_LayoutExample' => 1
     ]
 ];
