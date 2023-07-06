ecc30e83ccdcaa25714284e7b9ded113f6588c76
Gayathri Aravind <gayathri.aravind@ziffity.com>
2023-07-06 17:54:09 +0530

    Magento BlogManager Task - Sorting and Filter

--- app/code/Tasks/BlogManager/Block/BlogList.php
+++ app/code/Tasks/BlogManager/Block/BlogList.php
@@ -1,22 +1,35 @@
 <?php
 namespace Tasks\BlogManager\Block;
 
+use Magento\Customer\Model\SessionFactory;
+
 class BlogList extends \Magento\Framework\View\Element\Template
 {
-    public $blogCollection;
+    protected $blogCollection;
+    protected $customerSession;
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Tasks\BlogManager\Model\ResourceModel\Blog\CollectionFactory $blogCollection,
+        SessionFactory $customerSession,
         array $data = []
     ) {
         $this->blogCollection = $blogCollection;
+        $this->customerSession = $customerSession;
         parent::__construct($context, $data);
     }
 
     public function getBlogs()
     {
+        $customerId = $this->customerSession->create()->getCustomer()->getId();
+
+        // select * from blogmanager_blog where user_id = $customerId;
         $collection = $this->blogCollection->create();
+        $collection->addFieldToFilter('user_id', ['eq'=>$customerId])
+                   ->setOrder('updated_at', 'DESC'); // ORDER BY updated_at DESC
+
+                //    $collection->getSelect();
+
         return $collection;
     }
 }
